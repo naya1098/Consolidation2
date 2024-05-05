4ab8b3e9fcb5638d5ef7f847acde5871b8adbb78
naya1098 <167812849+naya1098@users.noreply.github.com>
2024-05-05 17:12:27 -0400

    Update consolidationproject2.py
    added question one

--- consolidationproject2.py
+++ consolidationproject2.py
@@ -17,9 +17,25 @@ gametwo.display_rules()
 def welcome():
     print('Welcome to the build your cat simulator ' + user_name + '!')
     print('----------------------------')
-    print('Keep in mind you only have $50 to cusomize your cat!')
+    print('Keep in mind you only have $100 to customize your cat!')
     print(' ')
     print('So use them wisely!! :3 ')
 
 # Global money variable
 money = 0
+
+#first question!
+def question_one():
+    global money
+    while True:
+        try:
+            ans = input(' Q1: Would you rather pay $50 for a long hair cat (1) or $10 for a short haired (2) ? Type 1 or 2 to pick: ')
+            if ans.lower() not in ('1', '2'):
+                raise ValueError(f"Please enter '1' or '2'. ")
+            break
+        except ValueError as e:
+            print(e)
+
+welcome()
+question_one()
+
